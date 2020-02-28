5dbe28f3797d1f970a7be65dd8fd0f00c8efeeb9
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-02-28 10:09:45 -0600

    token ignore

--- src/main/java/mx/ipn/escom/neuropsiauthserver/entity/OauthAccessToken.java
+++ src/main/java/mx/ipn/escom/neuropsiauthserver/entity/OauthAccessToken.java
@@ -2,9 +2,13 @@ package mx.ipn.escom.neuropsiauthserver.entity;
 
 import java.io.Serializable;
 import java.sql.Blob;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,21 +18,23 @@ import lombok.NoArgsConstructor;
 @Table(name = "oauth_access_token")
 public class OauthAccessToken implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  @Id
-  private String authenticationId;
+	@Id
+	private String authenticationId;
 
-  private String tokenId;
+	private String tokenId;
 
-  private Blob token;
+	@JsonIgnore
+	private Blob token;
 
-  private String userName;
+	private String userName;
 
-  private String clientId;
+	private String clientId;
 
-  private Blob authentication;
+	@JsonIgnore
+	private Blob authentication;
 
-  private String refreshToken;
+	private String refreshToken;
 
 }
